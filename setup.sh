3221e17997d7b3d3fe30be82ec2a7e52a6f4a018
edu <eduardo.navajascortes@gmail.com>
2024-10-26 16:00:31 +0200

    feat: modify picom configuration and setup script order
    - Update the picom configuration to use a different blur method and remove corner radius.
    - Refactor the setup script to install fonts, Kitty, and zsh configurations after installing Polybar.
    - Modify the script to ensure FZF is properly configured after installing zsh and fonts.
    - Update the script to install nvm and Kitty after installing Polybar.
    - The updated setup script now installs the wallpaper before installing Polybar.
    - Adjust the script to fix BurpSuite issues before installing zsh.

--- setup.sh
+++ setup.sh
@@ -18,24 +18,25 @@ source "$SCRIPT_DIR/code/install_picom.sh"
 
 echo "Setting up fonts..."
 
-run_as_root "source $SCRIPT_DIR/code/install_fonts.sh"
-update_progress
-
-run_as_user "source $SCRIPT_DIR/code/install_kitty.sh"
-
 run_as_user "source $SCRIPT_DIR/code/install_wallpaper.sh"
 
 run_as_user "source $SCRIPT_DIR/code/install_polybar.sh"
 
-run_as_root "source $SCRIPT_DIR/code/zsh_configs.sh"
+run_as_user "source $SCRIPT_DIR/code/install_fzf.sh"
+
+run_as_user "source $SCRIPT_DIR/code/install_nvm.sh"
+
+run_as_user "source $SCRIPT_DIR/code/install_kitty.sh"
 
 run_as_user "source $SCRIPT_DIR/code/fix_burpsuite.sh"
 
-run_as_user "source $SCRIPT_DIR/code/install_fzf.sh"
+run_as_root "source $SCRIPT_DIR/code/install_fonts.sh"
+update_progress
+
+run_as_root "source $SCRIPT_DIR/code/zsh_configs.sh"
 
 run_as_root "source $SCRIPT_DIR/code/install_fzf.sh"
 
-run_as_user "source $SCRIPT_DIR/code/install_nvm.sh"
 
 zsh
 source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
