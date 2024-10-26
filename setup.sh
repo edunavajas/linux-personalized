e9f44630a7a0a871a3e3c132e6f42fa2f7f377ce
edu <eduardo.navajascortes@gmail.com>
2024-10-26 12:44:05 +0200

    chore(setup): Update script paths to use $SCRIPT_DIR
    This commit updates the setup script to use the $SCRIPT_DIR variable for all script paths. This ensures that the script can be run from any directory without breaking.

--- setup.sh
+++ setup.sh
@@ -2,50 +2,39 @@
 
 find . -type f -name "*.sh" -exec chmod +x {} \;
 
-source ./code/common.sh
+source "$SCRIPT_DIR/code/common.sh"
 
 # Check if the script is being run as root
 check_sudo
 
-source ./code/install_update_dependencies.sh
+source "$SCRIPT_DIR/code/install_update_dependencies.sh"
 
 #echo "Changing to home directory..."
 #cd
 
-cd $SCRIPT_DIR
-source ./code/install_sxhkdrc.sh
+source "$SCRIPT_DIR/code/install_sxhkdrc.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_bspwm.sh
+source "$SCRIPT_DIR/code/install_bspwm.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_picom.sh
+source "$SCRIPT_DIR/code/install_picom.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_fonts.sh
+source "$SCRIPT_DIR/code/install_fonts.sh"
 
 echo "Setting up fonts..."
 
-cd $SCRIPT_DIR
-source ./code/install_kitty.sh
+source "$SCRIPT_DIR/code/install_kitty.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_wallpaper.sh
+source "$SCRIPT_DIR/code/install_wallpaper.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_polybar.sh
+source "$SCRIPT_DIR/code/install_polybar.sh"
 
-cd $SCRIPT_DIR
-source ./code/zsh_configs.sh
+source "$SCRIPT_DIR/code/zsh_configs.sh"
 
-cd $SCRIPT_DIR
-source ./code/fix_burpsuite.sh
+source "$SCRIPT_DIR/code/fix_burpsuite.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_fzf.sh
+source "$SCRIPT_DIR/code/install_fzf.sh"
 
-cd $SCRIPT_DIR
-source ./code/install_nvm.sh
+source "$SCRIPT_DIR/code/install_nvm.sh"
 
 echo -ne "\nScript completed successfully.\n"
 
