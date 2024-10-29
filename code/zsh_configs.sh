fb34e9f5a3f63c01c5ff1a71e2c050e01355111b
edu <eduardo.navajascortes@gmail.com>
2024-10-29 23:55:23 +0100

    fix link .p10k.zsh

--- code/zsh_configs.sh
+++ code/zsh_configs.sh
@@ -11,7 +11,7 @@ touch /home/$DEFAULT_USER/.zshrc
 envsubst < "$SCRIPT_DIR/system/.zshrc" > /home/$DEFAULT_USER/.zshrc
 touch /home/$DEFAULT_USER/.p10k.zsh
 cp -f "$SCRIPT_DIR/system/.p10k.zsh" /home/$DEFAULT_USER/.p10k.zsh
-ln -s -f /home/$DEFAULT_USER/.p10k.zsh
+ln -s -f /home/$DEFAULT_USER/.p10k.zsh ~/.p10k.zsh
 ln -s -f /home/$DEFAULT_USER/.zshrc ~/.zshrc
 mkdir -p /usr/share/zsh-autocomplete
 cd /usr/share/zsh-autocomplete
