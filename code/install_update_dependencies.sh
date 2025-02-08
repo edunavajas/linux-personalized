0d11557c3fd12db3e6780ff379ddaa8593c3a208
edu <eduardo.navajascortes@gmail.com>
2025-02-08 02:37:35 +0100

    chore(dependencies): Add and update package installation configurations

--- code/install_update_dependencies.sh
+++ code/install_update_dependencies.sh
@@ -4,6 +4,8 @@ source "$SCRIPT_DIR/code/common.sh"
 
 source "$SCRIPT_DIR/code/upgrade_system.sh"
 
+run_command "Updating package lists" "apt-get update -y"
+
 run_command "Installing build-essential and related packages" "apt install -y build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev"
 
 run_command "Installing more XCB development packages" "apt install -y libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev"
@@ -12,7 +14,7 @@ run_command "Installing additional XCB and related development packages" "apt in
 
 run_command "Installing XCB image and rendering packages" "apt install -y libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev"
 
-run_command "Installing development tools and applications" "apt install -y libxcb-xfixes0-dev meson ninja-build uthash-dev cmake polybar rofi zsh imagemagick feh zsh-autosuggestions"
+run_command "Installing development tools and applications" "apt install --fix-missing -y libxcb-xfixes0-dev meson ninja-build uthash-dev cmake polybar rofi zsh imagemagick feh zsh-autosuggestions"
 
 run_command "Installing zsh syntax highlighting and other utilities" "apt install -y zsh-syntax-highlighting ranger xcb-proto libxcb-ewmh-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util-dev"
 
--- code/upgrade_system.sh
+++ code/upgrade_system.sh
@@ -9,7 +9,7 @@ run_command "Commenting out the first line of $SOURCE_LIST" "sed -i '1 s/^/#/' $
 run_command "Updating package lists" "apt update -y"
 
 if grep -qi "parrot" /etc/os-release; then
-    run_command "Running parrot-upgrade" "parrot-upgrade"
+    run_command "Running parrot-upgrade" "parrot-upgrade -y"
 else
     run_command "Upgrading packages" "apt upgrade -y"
 fi
